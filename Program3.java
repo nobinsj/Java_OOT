24bc4f9b6145f9166718b859a6a8e27df80491f5
nobinsj <nobinsjohns1@gmail.com>
2021-05-24 22:53:06 +0530

    program 3

---
+++ Program3.java
@@ -0,0 +1,10 @@
+
+public class Program3 {
+
+	public static void main(String args[]) {
+		
+		System.out.println("******\n*****\n***\n*\n***\n*****\n*******");
+		System.out.println("\n\n\n");
+		System.out.println("**********\n*\t*\n*\t*\n*\t*\n**********");
+	}
+}
