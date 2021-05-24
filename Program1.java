d56c861da3449b92d2adb70868cacbaa2f0b944b
nobinsj <nobinsjohns1@gmail.com>
2021-05-24 20:49:14 +0530

    Program1 Print_details

---
+++ Program1.java
@@ -0,0 +1,12 @@
+public class Program1 {
+	
+	public static void main (String args[]) {
+		System.out.println("NAME\tEMAIL\t\tEMPLOYEE-CODE\tPHONE\n"
+				+ "Umesh\tumesh@cse\tpo3161\t\t25764728\n"
+				+ "Salil\tsalil@cse\tpo3160\t\t25764729\n"
+				+ "Akhil\takhil@it\tpo3159\t\t25764730\n"
+				+ "Kevin\tkevin@it\tpo3158\t\t25764731\n"
+				+ "Abhi\tabhi@it\t\tpo3157\t\t25764732\n");
+	}
+
+}
