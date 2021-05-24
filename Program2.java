20584fbae58bed64d5a3e9e0d9bc9ce661490efd
nobinsj <nobinsjohns1@gmail.com>
2021-05-24 21:04:57 +0530

    Program2 added

---
+++ Program2.java
@@ -0,0 +1,7 @@
+
+public class Program2 {
+	public static void main(String arg[]) {
+		System.out.println("\"Can we print '\' with System.out.println() statement?\"");
+	}
+}
+//Output => "Can we print '\' with System.out.println() statement?"
