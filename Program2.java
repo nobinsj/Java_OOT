05009bf238614348ad17e016952f8187643f89ee
nobinsj <nobinsjohns1@gmail.com>
2021-05-24 21:20:25 +0530

    Program2 Print...

--- Program2.java
+++ Program2.java
@@ -1,7 +1,7 @@
 
 public class Program2 {
 	public static void main(String arg[]) {
-		System.out.println("\"Can we print '\' with System.out.println() statement?\"");
+		System.out.println("\"Can we print '\\' with System.out.println() statement?\"");
 	}
 }
 //Output => "Can we print '\' with System.out.println() statement?"
