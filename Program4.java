34fd9b5210bd575b0851a75f49d7e556624eec2a
nobinsj <nobinsjohns1@gmail.com>
2021-05-24 23:02:04 +0530

    Program 3 ---number

---
+++ Program4.java
@@ -0,0 +1,19 @@
+
+public class Program4 {
+
+	public static void main(String args[]) {
+		int a_number = 4;
+		
+		if(a_number==1)
+			System.out.println("One");
+		else if(a_number==2)
+			System.out.println("Two");
+		else if(a_number==3)
+			System.out.println("Three");
+		else if(a_number==4)
+			System.out.println("Four");
+		else if(a_number==5)
+			System.out.println("Five");
+			
+	}
+}
